da8f931e15690ad53ea7ac47a00ae4d02b7c80f4
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-05-22 17:46:42 +0300

    passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib = [0, 1]
+        for i in range(2, length):
+            fib.append(fib[i-1] + fib[i-2])
+        print(fib)
